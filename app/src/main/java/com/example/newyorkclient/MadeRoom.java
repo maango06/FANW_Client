376d5a64fe2f101739ace7294b7f5759aeef6932
Mohand <68048609+maango06@users.noreply.github.com>
2021-05-19 15:59:20 +0900

    network message finish

--- app/src/main/java/com/example/newyorkclient/MadeRoom.java
+++ app/src/main/java/com/example/newyorkclient/MadeRoom.java
@@ -1,15 +1,23 @@
 package com.example.newyorkclient;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MadeRoom extends AppCompatActivity {
     TextView player1;
@@ -19,12 +27,18 @@ public class MadeRoom extends AppCompatActivity {
     TextView player5;
     TextView player6;
     TextView codename;
+    TextView chat_log;
+
+    Button sending_button;
+    EditText sending_text;
 
     Button cancel;
     Button start;
 
     boolean master = false;
     String room_code = null;
+    MadeRoom_thread maderoom_thread = null;
+    MadeRoom_handler handler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,9 @@ public class MadeRoom extends AppCompatActivity {
         codename = findViewById(R.id.Codename);
         cancel = findViewById(R.id.cancel);
         start = findViewById(R.id.start);
+        sending_button = findViewById(R.id.send_button);
+        sending_text = findViewById(R.id.sending_text);
+        chat_log = findViewById(R.id.chat_log);
 
         cancel.setEnabled(false);
         start.setEnabled(false);
@@ -48,6 +65,8 @@ public class MadeRoom extends AppCompatActivity {
         master = now_intent.getBooleanExtra("master", false);
 
         codename.setText(room_code);
+
+        handler = new MadeRoom_handler();
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +95,40 @@ public class MadeRoom extends AppCompatActivity {
             }
         });
 
+        sending_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = sending_text.getText().toString();
+                sending_text.setText("");
+                new send_thread("message/" + text).start();
+            }
+        });
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        maderoom_thread.set_stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        maderoom_thread = new MadeRoom_thread(handler);
+        maderoom_thread.start();
+    }
+
+    class MadeRoom_handler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            String line = msg.getData().getString("value");
+            String[] info = line.split("/");
+            switch(info[0]) {
+                case "message":
+                    String str = chat_log.getText() + info[1] + " : " + info[2] + "\n";
+                    chat_log.setText(str);
+                    break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/newyorkclient/MadeRoom_thread.java
@@ -0,0 +1,48 @@
+package com.example.newyorkclient;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class MadeRoom_thread extends Thread{
+
+    Handler handler = null;
+    boolean stop = false;
+
+    MadeRoom_thread(Handler _handler) {
+        this.handler = _handler;
+    }
+
+    public void set_stop() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        socket_queue que = GlobalApplication.getGlobalApplicationContext().getQue();
+        String line = null;
+        while(true) {
+            if(this.stop) break;
+            line = que.pop();
+            if(line == null) {
+                Thread.yield();
+                continue;
+            }
+            Log.v("Lobby_thread", line);
+
+            String[] info = line.split("/");
+            switch(info[0]) {
+                case "message":
+                    Message msg = handler.obtainMessage();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("value", line);
+                    msg.setData(bundle);
+                    handler.sendMessage(msg);
+                    break;
+                default:
+                    Log.e("Lobby", "unknown" + line);
+            }
+        }
+    }
+}
