292e355c66fe100cefcce08b55eb7f717d13b364
Mohand <68048609+maango06@users.noreply.github.com>
2021-06-21 14:45:29 +0900

    bug fix

--- app/src/main/java/com/example/newyorkclient/MadeRoom.java
+++ app/src/main/java/com/example/newyorkclient/MadeRoom.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -65,6 +66,7 @@ public class MadeRoom extends AppCompatActivity {
         sending_button = findViewById(R.id.sending_button);
         sending_text = findViewById(R.id.sending_text);
         chat_log = findViewById(R.id.chat_log);
+        chat_log.setMovementMethod(new ScrollingMovementMethod());
 
         Intent now_intent = getIntent();
         room_code = now_intent.getStringExtra("room_code");
